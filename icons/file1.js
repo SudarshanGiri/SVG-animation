fc9b7dfd1a962fb1d962ea6f55002084452f9e55
Sudarshan Giri <sudarshanhang@gmail.com>
2021-06-03 23:01:00 +0545

    initial commit

---
+++ icons/file1.js
@@ -0,0 +1,5 @@
+if (document.getElementById("om-ijsv0oi624u2gpf6jbx2-optin")) {
+  document.querySelector(".entry-content-single .wp-block-image:nth-child(3)").classList.add("fade-1");
+  document.querySelector(".entry-content-single p:nth-child(4)").classList.add("fade-2");
+  document.querySelector(".entry-content-single p:nth-child(5), .entry-content-single blockquote:nth-child(5) ").classList.add("fade-3");
+};
